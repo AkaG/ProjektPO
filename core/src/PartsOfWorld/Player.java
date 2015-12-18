21787e65284c1b1422dd400fe6813f3621787146
Adas <cwianek@gmail.com>
2015-12-18 14:05:06 +0100

    zmieony sprite

--- core/src/PartsOfWorld/Player.java
+++ core/src/PartsOfWorld/Player.java
@@ -11,136 +11,125 @@ import com.badlogic.gdx.math.Vector2;
 
 import interfaces.IPlayerControls;
 
+public class Player extends MovingObject implements InputProcessor, IPlayerControls {
 
-/*
- * 
- * 	NAPISANA TYLKO JAKO PRZYKLAD (w sumie ja tego nie pisa³em)
- * 
- */
-
-public class Player extends MovingObject implements InputProcessor, IPlayerControls{
-	
 	public static final int width = 50;
 	public static final int height = 50;
 
 	private boolean canJump;
 	private float gravity;
-	public enum Direction {RUN_LEFT, RUN_RIGHT, STAY_LEFT, STAY_RIGHT}; //nie dodalem jumpa, bo wtedy nie mozna //by bylo skakac po skosie
-	Direction dir;														
-	
-	private TextureAtlas textureAtlas;
-    private Animation GoLeftAnimation;
-    private Animation GoRightAnimation;
-    private Animation StayRightAnimation;
-    private Animation StayLeftAnimation;
-    private Animation JumpAnimation;
-    private float elapsedTime = 0;
-		
-    private Gun gun;
-    
-    
-    
+
+	public enum Direction {
+		RUN_LEFT, RUN_RIGHT, STAY_LEFT, STAY_RIGHT
+	}; // nie dodalem jumpa, bo wtedy nie mozna //by bylo skakac po skosie
+
+	Direction dir;
+
+	protected TextureAtlas textureAtlas;
+	protected Animation GoAnimation;
+	protected Animation StayAnimation;
+	protected Animation JumpAnimation;
+	protected float elapsedTime = 0;
+
+	private Gun gun;
+
 	public Player(float x, float y) {
-		
-        this.x = Gdx.graphics.getWidth()/2 - this.width/2;
-        this.y = 0;
-        
-        this.canJump = true;
-        this.xSpeed = 300;
-        this.ySpeed = 0;
-        this.gravity = -1500; //takie wartoœci bo jest mno¿one przez delte(¿eby na ka¿dym kompie dzia³alo tak samo)
-        				//ale nie wiem jak to zrobiæ lepiej 
-        
-        textureAtlas = new TextureAtlas(Gdx.files.internal("contra.atlas"));
-        
-        GoLeftAnimation = new Animation(0.1f,
-                (textureAtlas.findRegion("1")),
-                (textureAtlas.findRegion("2")),
-                (textureAtlas.findRegion("3")),
-                (textureAtlas.findRegion("4")),
-                (textureAtlas.findRegion("5"))
-               );
-        
-        GoRightAnimation = new Animation(0.1f,
-                (textureAtlas.findRegion("6")),
-                (textureAtlas.findRegion("7")),
-                (textureAtlas.findRegion("8")),
-                (textureAtlas.findRegion("9")),
-                (textureAtlas.findRegion("10"))
-               );
-        
-        StayLeftAnimation = new Animation(0.1f,(textureAtlas.findRegions("4")));
-        StayRightAnimation = new Animation(0.1f,(textureAtlas.findRegions("7")));
-        
-        this.dir = Direction.STAY_LEFT;
-        
-        this.gun = new Gun(this);
-        
-    }
-	
-    public void update(float delta) {
-    	
-    	movement(delta);
-    	elapsedTime += delta;
-    	
-    	//POCISKI
-    	for(Bullet b: gun.getBullets())
-    	{
-    		b.move();
-    	}
-    	
-    }
-    
-   //w movement jest zrobiona grawitacja(sciaga playera w dol) i poruszanie w lewo/prawo  
-   
-    public void movement(float delta) {
-    	
-    	y += ySpeed * delta; //œci¹ga go w dó³
-    	
-    	if(y > 0){ //jezeli jest na ekranie
-    		ySpeed += gravity * delta; //dodajemy grawitacje zeby zwiekszyæ prêdkoœæ spadania
-    	}
-    	else{
-    		y = 0; //zeby nie spadl ponizej ekranu
-    		ySpeed = 0;
-    		canJump = true;
-    	}
-    	
-    	if(dir == Direction.RUN_LEFT) moveRight();
-    	else if(dir == Direction.RUN_RIGHT)  moveLeft();
-    	
-    }
-    
-    private void jump(){
-    	if(canJump && ySpeed >= -100)
-    	ySpeed += 800; // im wieksza wartosc tym wyzej skoczy
-    	canJump = false;
-    }
-    
-  
-    public void draw(SpriteBatch batch){
-    	switch(dir)
-     	{
-     	case RUN_RIGHT:
-     		batch.draw(GoRightAnimation.getKeyFrame(elapsedTime, true), x, y);
-     		break;
-     	case RUN_LEFT:
-     		batch.draw(GoLeftAnimation.getKeyFrame(elapsedTime, true), x,y);
-     		break;
-     	case STAY_LEFT:
-     		batch.draw(StayLeftAnimation.getKeyFrame(elapsedTime, true), x, y);
-     		break;
-     	case STAY_RIGHT: 
-     		batch.draw(StayRightAnimation.getKeyFrame(elapsedTime, true), x, y);
-     		break;
-     	}
-    }
-    
-    
-    private void moveLeft() {
+
+		this.x = Gdx.graphics.getWidth() / 2 - this.width / 2;
+		this.y = 0;
+
+		this.canJump = true;
+		this.xSpeed = 300;
+		this.ySpeed = 0;
+		this.gravity = -1500; // takie wartoœci bo jest mno¿one przez delte(¿eby
+		// na ka¿dym kompie dzia³alo tak samo)
+		// ale nie wiem jak to zrobiæ lepiej
+
+		textureAtlas = new TextureAtlas(Gdx.files.internal("sprite2.atlas"));
+		GoAnimation = new Animation(0.1f, (textureAtlas.findRegion("1")), (textureAtlas.findRegion("2")),
+				(textureAtlas.findRegion("3")));
+
+		StayAnimation = new Animation(0.1f, (textureAtlas.findRegions("4")));
+
+		JumpAnimation = new Animation(0.5f, (textureAtlas.findRegion("8")));
+
+		this.dir = Direction.STAY_LEFT;
+
+		this.gun = new Gun(this);
+
+	}
+
+	public void update(float delta) {
+
+		movement(delta);
+		elapsedTime += delta;
+
+		// POCISKI
+		for (Bullet b : gun.getBullets()) {
+			b.move();
+		}
+
+	}
+
+	// w movement jest zrobiona grawitacja(sciaga playera w dol) i poruszanie w
+	// lewo/prawo
+
+	public void movement(float delta) {
+
+		y += ySpeed * delta; // œci¹ga go w dó³
+
+		if (y > 0) { // jezeli jest na ekranie
+			ySpeed += gravity * delta; // dodajemy grawitacje zeby zwiekszyæ
+			// prêdkoœæ spadania
+		} else {
+			y = 0; // zeby nie spadl ponizej ekranu
+			ySpeed = 0;
+			canJump = true;
+		}
+
+		if (dir == Direction.RUN_LEFT)
+			moveRight();
+		else if (dir == Direction.RUN_RIGHT)
+			moveLeft();
+
+	}
+
+	private void jump() {
+		if (canJump && ySpeed >= -100)
+			ySpeed += 800; // im wieksza wartosc tym wyzej skoczy
+		canJump = false;
+	}
+
+	public void draw(SpriteBatch batch) {
+		if (canJump) { // jak moze skakaæ to znaczy, ¿e nie jest w powietrzu,
+			// narazie nie zmieniam
+
+			switch (dir) {
+			case RUN_RIGHT:
+				batch.draw(GoAnimation.getKeyFrame(elapsedTime, true), x - width / 2, y, width, height);
+				break;
+			case RUN_LEFT:
+				batch.draw(GoAnimation.getKeyFrame(elapsedTime, true), x + width / 2, y, -width, height);
+				break;
+			case STAY_LEFT:
+				batch.draw(StayAnimation.getKeyFrame(elapsedTime, true), x + width / 2, y, -width, height);
+				break;
+			case STAY_RIGHT:
+				batch.draw(StayAnimation.getKeyFrame(elapsedTime, true), x - width / 2, y, width, height);
+				break;
+			}
+		} else {
+			if (dir == Direction.STAY_RIGHT || dir == Direction.RUN_RIGHT) {
+				batch.draw(JumpAnimation.getKeyFrame(elapsedTime, false), x - width / 2, y, width, height);
+			} else if (dir == Direction.STAY_LEFT || dir == Direction.RUN_LEFT)
+				batch.draw(JumpAnimation.getKeyFrame(elapsedTime, false), x + width / 2, y, -width, height);
+		}
+	}
+
+	private void moveLeft() {
 		// TODO Auto-generated method stub
-    	x += xSpeed * Gdx.graphics.getDeltaTime();
-    	
+		x += xSpeed * Gdx.graphics.getDeltaTime();
+
 	}
 
 	private void moveRight() {
@@ -153,39 +142,38 @@ public class Player extends MovingObject implements InputProcessor, IPlayerContr
 		// TODO Auto-generated method stub
 		gun.shoot();
 	}
-	
+
 	@Override
 	public void Pjump() {
 		jump();
 	}
-	
+
 	@Override
 	public void PmoveLeft() {
 		// TODO Auto-generated method stub
 		dir = Direction.RUN_LEFT;
 	}
-	
+
 	@Override
 	public void PmoveRight() {
 		// TODO Auto-generated method stub
 		dir = Direction.RUN_RIGHT;
 	}
-	
+
 	@Override
 	public boolean keyDown(int keycode) {
 		// TODO Auto-generated method stub
-		if(keycode == Input.Keys.LEFT){
+		if (keycode == Input.Keys.LEFT) {
 			PmoveLeft();
-		}
-		else if(keycode == Input.Keys.RIGHT){
+		} else if (keycode == Input.Keys.RIGHT) {
 			PmoveRight();
 		}
-		
-		if(keycode == Input.Keys.UP){
+
+		if (keycode == Input.Keys.UP) {
 			Pjump();
 		}
-		
-		if(keycode == Input.Keys.SPACE){
+
+		if (keycode == Input.Keys.SPACE) {
 			Pshoot();
 		}
 		return false;
@@ -194,11 +182,11 @@ public class Player extends MovingObject implements InputProcessor, IPlayerContr
 	@Override
 	public boolean keyUp(int keycode) {
 		// TODO Auto-generated method stub
-		if(dir != Direction.RUN_RIGHT && keycode == Input.Keys.LEFT)
+		if (dir != Direction.RUN_RIGHT && keycode == Input.Keys.LEFT)
 			dir = Direction.STAY_LEFT;
-		if(dir != Direction.RUN_LEFT && keycode == Input.Keys.RIGHT)
+		if (dir != Direction.RUN_LEFT && keycode == Input.Keys.RIGHT)
 			dir = Direction.STAY_RIGHT;
-		
+
 		return false;
 	}
 
@@ -237,17 +225,11 @@ public class Player extends MovingObject implements InputProcessor, IPlayerContr
 		// TODO Auto-generated method stub
 		return false;
 	}
-    
-    
-    
-    
-	
 
-//////////////////////////////////////GETTERY I SETTERY
-	
-	
+	////////////////////////////////////// GETTERY I SETTERY
+
 	public Direction getDir() {
-		
+
 		return dir;
 	}
 
@@ -263,14 +245,12 @@ public class Player extends MovingObject implements InputProcessor, IPlayerContr
 		this.dir = dir;
 	}
 
-	
 	public boolean isCanJump() {
-	return canJump;
+		return canJump;
 	}
 
 	public void setCanJump(boolean canJump) {
-	this.canJump = canJump;
+		this.canJump = canJump;
 	}
-	
-}
 
+}
