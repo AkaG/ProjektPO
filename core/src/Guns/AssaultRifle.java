195cec2572ca1ec9071d16fd4b8a936ecb019eb7
AkaG <kuba0095@gmail.com>
2016-01-17 15:38:32 +0100

    naprawiony merge

--- core/src/Guns/AssaultRifle.java
+++ core/src/Guns/AssaultRifle.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package Guns;
 
 import com.badlogic.gdx.graphics.Texture;
@@ -22,28 +21,3 @@ public class AssaultRifle extends Gun{
 
 	
 }
-=======
-package Guns;
-
-import com.badlogic.gdx.graphics.Texture;
-
-import PartsOfWorld.Player;
-
-public class AssaultRifle extends Gun{
-
-	public AssaultRifle(Player player) {
-		super(player);
-		// TODO Auto-generated constructor stub
-		timePerShoot = 0.15f;
-		bulletSpeed = 500;
-		damage = 5;
-		
-		width = 30;
-		height = 15;
-		gunTexture = new Texture("m4a1.png");
-		
-	}
-
-	
-}
->>>>>>> bce19af568227add24c7f80d2def0ce00e558cb9
--- core/src/Guns/Pistol.java
+++ core/src/Guns/Pistol.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 package Guns;
 
 import com.badlogic.gdx.graphics.Texture;
@@ -22,28 +21,3 @@ public class Pistol extends Gun{
 
 	
 }
-=======
-package Guns;
-
-import com.badlogic.gdx.graphics.Texture;
-
-import PartsOfWorld.Player;
-
-public class Pistol extends Gun{
-
-	public Pistol(Player player) {
-		super(player);
-		
-		timePerShoot = 0.5f;
-		bulletSpeed = 500;
-		damage = 10;
-		
-		width = 20;
-		height = 5;
-		gunTexture = new Texture("pistol.png");
-		
-	}
-
-	
-}
->>>>>>> bce19af568227add24c7f80d2def0ce00e558cb9
