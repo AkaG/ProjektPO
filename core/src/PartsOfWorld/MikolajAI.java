5c3bd6f5bc9722565da90b730e2eed5667e5b877
Mateusz Krasicki <mateusz.krasicki@gmail.com>
2016-01-18 09:45:47 +0100

    mikołajAI

--- core/src/PartsOfWorld/MikolajAI.java
+++ core/src/PartsOfWorld/MikolajAI.java
@@ -3,9 +3,13 @@ package PartsOfWorld;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import java.util.Random;
 
 public class MikolajAI extends AIPlayer {
 
+	private float time = 0;
+	Random rand = new Random();
+	
 	public MikolajAI(float x, float y, TypeOfGun gun) {
 		super(x, y, gun);
 		initTextures();
@@ -25,7 +29,31 @@ public class MikolajAI extends AIPlayer {
 	@Override
 	public void AIUpdate(float delta) {
 		// TODO Auto-generated method stub
-
+		int tmp = rand.nextInt(20);
+		
+		time += delta;
+		if (time >= 1) {
+			randomMove();
+			time = 0;
+		}
+		if (tmp == 19) AIjump(); 
+		else this.AIshoot();
 	}
+	
+	public void randomMove() {
+		int tmp = rand.nextInt(2);
 
+		
+		if(this.x <= 5) {
+			this.AImoveRight();
+		} else if (this.x > 800) {
+			this.AImoveLeft();
+		} else {
+			switch(tmp){
+			case 0 : this.AImoveLeft(); break;
+			case 1 : this.AImoveRight(); break;
+			default : break;
+			}
+		}
+	}
 }
