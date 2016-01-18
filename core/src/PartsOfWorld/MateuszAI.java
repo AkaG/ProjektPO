af6649ac9ef228c6988c1c24a0d3624bb52fbab1
Mateusz Krasicki <mateusz.krasicki@gmail.com>
2016-01-18 07:42:01 +0100

    Add MateuszAI

--- core/src/PartsOfWorld/MateuszAI.java
+++ core/src/PartsOfWorld/MateuszAI.java
@@ -11,6 +11,9 @@ public class MateuszAI extends AIPlayer {
 
 	Random random;
 	private float timer = 0;
+	private boolean movingLeft;
+	private boolean movingRight;
+	private boolean jumping;
 	
 	public MateuszAI(float x, float y, TypeOfGun gun) {
 		super(x, y, gun);
@@ -33,12 +36,29 @@ public class MateuszAI extends AIPlayer {
 	@Override
 	public void AIUpdate(float delta) {
 		// TODO Auto-generated method stub
+		timer += delta;
+		if (timer >= 0.5){
+			randomMove();
+			timer = 0;
+		}
+		AIshoot();
 		
+		if(this.x >= Gdx.graphics.getWidth() - this.width) AImoveLeft();
+		else if(this.x <= 0) AImoveRight();
 	}
 
 	public void randomMove()
 	{
-	
+		switch (random.nextInt(3)){
+		case 0:
+			AImoveLeft();
+			break;
+		case 1:
+			AImoveRight();
+			break;
+		case 2:
+			AIjump();
+			break;
+		}
 	}
-
 }
